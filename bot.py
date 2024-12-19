c27038cfcf3e8d0691aead7c924285d83c96eb89
omid-gg <github.omid.gg@gmail.com>
2024-12-19 17:36:26 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+from telegram.ext import Updater, CommandHandler
+
+# دستور /start
+def start(update, context):
+    update.message.reply_text("سلام! به Mystery Land خوش اومدی. آماده ماجراجویی هستی؟")
+
+# دستور /help
+def help_command(update, context):
+    update.message.reply_text("دستورات: \n/start - شروع بازی \n/help - راهنما")
+
+# توکن رباتت رو اینجا بذار
+TOKEN = "اینجا توکن ربات خودت رو بذار"
+
+# تنظیم ربات
+updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
+
+# اضافه کردن دستورات
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("help", help_command))
+
+# اجرای ربات
+updater.start_polling()
+updater.idle()
