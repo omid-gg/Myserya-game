87ffc801d9ec2d8d420e3b66901eef945ca33972
omid-gg <github.omid.gg@gmail.com>
2024-12-19 18:33:20 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,16 @@
 from telegram.ext import Updater, CommandHandler
 
-# دستور /start
-def start(update, context):
-    update.message.reply_text("سلام! به Mystery Land خوش اومدی. آماده ماجراجویی هستی؟")
-
-# دستور /help
-def help_command(update, context):
-    update.message.reply_text("دستورات: \n/start - شروع بازی \n/help - راهنما")
-
-# توکن رباتت رو اینجا بذار
 TOKEN = "7869614206:AAFKyhtNjeb_nRM883nnrPoROScjkSNtUfc"
 
-# تنظیم ربات
-updater = Updater(TOKEN)
-dispatcher = updater.dispatcher
+def start(update, context):
+    update.message.reply_text("سلام! ربات تلگرام آماده است.")
 
-# اضافه کردن دستورات
-dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("help", help_command))
+if __name__ == "__main__":
+    updater = Updater(TOKEN, use_context=True)
 
-# اجرای ربات
-updater.start_polling()
-updater.idle()
+    # اضافه کردن دستور /start
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+
+    # شروع ربات
+    updater.start_polling()
+    updater.idle()
