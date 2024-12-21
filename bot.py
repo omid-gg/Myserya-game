cd978df071c208fa03df6934a039b46ca9a6fffa
omid-gg <github.omid.gg@gmail.com>
2024-12-21 12:23:57 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import os
 import logging
-from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import requests
@@ -14,6 +13,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# دستور استارت
 def start(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id  # دریافت چت آیدی از پیام
     update.message.reply_text(
@@ -27,16 +27,19 @@ app = Flask(__name__)
 
 @app.route('/start_game', methods=['POST'])
 def start_game():
-    chat_id = os.getenv("CHAT_ID") 
-    bot = requests.get(f'https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage', params={
-        'chat_id': chat_id, 
-        'text': 'بازی شروع شد! مبارزات آغاز می‌شود.'
-    })
-    if bot.status_code == 200:
-        return jsonify({"status": "success", "message": "Game started!"})
-    else:
-        return jsonify({"status": "error", "message": "Failed to start game"}), 400
+  
+    updater = Updater(TELEGRAM_API_TOKEN)
+    dispatcher = updater.dispatcher
+    for user in dispatcher.chat_data.keys():
+        chat_id = user 
+        bot = requests.get(f'https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage', params={
+            'chat_id': chat_id, 
+            'text': 'بازی شروع شد! مبارزات آغاز می‌شود.'
+        })
+        if bot.status_code != 200:
+            return jsonify({"status": "error", "message": "Failed to start game"}), 400
+    return jsonify({"status": "success", "message": "Game started!"})
 
 if __name__ == '__main__':
-    main()
-    app.run(host='0.0.0.0',port='5000')
+    # تنظیمات برای اجرا در حالت تولید
+    app.run(debug=False, host='0.0.0.0', port=5000)
